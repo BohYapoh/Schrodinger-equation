e923ec99dfe1748679547e452b30a4e7872d962e
Пето <you@example.com>
2025-08-07 03:50:40 +0300

    Ver 1

--- 11.py
+++ 11.py
@@ -1,5 +1,29 @@
-import main
-from main import make_diagonal
+
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 
-make_diagonal(H)
+def make_graph(psi, level, x):
+    Energy_level = level
+    plt.show()
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=x, y=psi[:, level], mode='lines', name='psi'))
+    fig.update_layout(title='График psi', xaxis_title='x', yaxis_title='y')
+    fig.show()
+
+
+nodes = 100
+x = np.linspace(-5 ,5, nodes)
+H = np.zeros((nodes,nodes))
+one_div_h_2 = 1/((10/(nodes-1))**2)
+for i in range(nodes):
+    for j in range(nodes):
+        if i == j:
+            H[i][j] = (1/2)*x[i]**2+ one_div_h_2
+        if j == i + 1 or j == i - 1:
+            H[i][j] = -one_div_h_2 *(1/2)
+E, psi = np.linalg.eigh(H)
+make_graph(psi, 5, x)
+print(E)
--- main.py
+++ main.py
@@ -27,15 +27,12 @@ def QR(A):
 
     return Q, R
 
-def make_diagonal(H):
+def make_diagonal(H, iter):
     H_new = H
     Q_total = np.zeros((20, 20))
-    for i in range(2000):
-        #os.system('cls')
+    for i in range(iter):
         Q, R = QR(H_new)
         H_new = R @ Q
-        #os.system('cls')
-        #print(H_new[1][0])
         if i == 0:
             Q_total = Q
         else:
@@ -43,52 +40,39 @@ def make_diagonal(H):
 
     return H_new, Q_total
 
+
+def make_graph(Q_total_norm, level, x):
+    Energy_level = level
+    plt.show()
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=x, y=Q_total_norm[Energy_level - 1], mode='lines', name='psi'))
+    fig.update_layout(title='График psi', xaxis_title='x', yaxis_title='y')
+    fig.show()
+
 if __name__ == "__main__":
-    dx = 1
-    nodes =  100
-    x = np.linspace(0 ,1, nodes + 2)
+    nodes = 30
     H = np.zeros((nodes,nodes))
+    one_div_h_2 = 1/((1/(nodes+1))**2)
     for i in range(nodes):
         for j in range(nodes):
-           if i == j:
+            if i == j:
                 H[i][j] = 2
-           if j == i + 1 or j == i - 1:
+            if j == i + 1 or j == i - 1:
                 H[i][j] = -1
-    one_div_h_2 = 1/((1/(nodes+1))**2)
+    x = np.linspace(0, 1, nodes + 2)
     H =H * one_div_h_2 *1/2
-    E, psi = np.linalg.eigh(H)
-    print(np.linalg.norm(psi[:,8]))
-    np.set_printoptions(precision=3,  suppress=True)
-    H, Q_total = make_diagonal(H)
-    H = np.round(H, decimals=10)
+    H, Q_total = make_diagonal(H, 1000)
     Q_total_norm = [[0 for _ in range(nodes)] for _ in range(nodes)]
     y = [[0 for _ in range(nodes+2)] for _ in range(nodes+2)]
-    print(Q_total_norm)
     for i in range(nodes):
         norm = np.sqrt(np.sum(np.abs(Q_total[:][i]) ** 2) * (1 / (nodes + 1)))
         Q_total_norm[i] = list(Q_total[:][i]/norm)
-        print(len(Q_total_norm[i]))
         Q_total_norm[i].append(0)
         Q_total_norm[i].insert(0, 0)
         for j in range(len(x)):
             y[i][j]=math.sqrt(2)*math.sin((i+1)*math.pi*x[j])
-            print(y[i])
-    print(Q_total_norm)
-    print(len(x))
-    Energy_level = 3
-    plt.show()
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=x, y=Q_total_norm[Energy_level - 1], mode='lines', name='psi'))
-    fig.add_trace(go.Scatter(x=x, y=y[Energy_level - 1], mode='lines', name='psi_anal'))
-    fig.update_layout(title='График psi', xaxis_title='x', yaxis_title='y')
-    fig.show()
+    make_graph(Q_total_norm,3, x)
     for i in range (nodes):
         for j in range(nodes):
             if i == j:
-                print(H[i][j])
-    for i in range (nodes):
-        print(E[i])
-
-
-
-
+                print(f"Уровень энергии {nodes - i} = ", H[i][j])
